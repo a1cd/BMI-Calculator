6cdfc2d960c2d8b9ad3d05396a2376894a10c2e1
a1cd <71281043+a1cd@users.noreply.github.com>
2021-11-06 21:59:56 -0400

    workflow stuff

--- src/main/java/BMICalculator.java
+++ src/main/java/BMICalculator.java
@@ -1,7 +1,11 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class BMICalculator {
+    static DecimalFormat DF = new DecimalFormat("0.00");
+
     public static double computeBMI(int inches, int pounds) {
+        if (inches<=0 || pounds<=0) return 0;
         double meters = 0.0254*inches;
         double kg = 0.454*pounds;
         return kg/Math.pow(meters, 2);
@@ -11,8 +15,7 @@ public class BMICalculator {
         QueryUser in = new QueryUser();
         String height = in.QueryString("Enter your height in feet and inches (format 6'2\"): ");
         int weight = in.QueryInt("Enter your weight in pounds: ");
-        System.out.println(height);
-        System.out.println(weight);
         StringParser parser = new StringParser(height);
+        System.out.println("Your BMI, expressed as weight(kg)/height(m)^2: " + DF.format(computeBMI(parser.getTotalInches(), weight)) + " kg/m^2");
     }
 }
